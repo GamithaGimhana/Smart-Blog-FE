4e4eb6ab46e3f7b0982523b67e1986e2a58564e4
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-11-10 12:55:39 +0530

    Add response interceptor to handle token refresh

--- src/services/api.ts
+++ src/services/api.ts
@@ -30,5 +30,33 @@ api.interceptors.request.use((config) => {
 // }
 // )
 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await axios.post(
+          'http://localhost:5000/api/v1/auth/refresh',
+          {},
+          { withCredentials: true }
+        );
+
+        const newToken = res.data.accessToken;
+        localStorage.setItem('accessToken', newToken);
+        originalRequest.headers.Authorization = ~`Bearer ${newToken}`;
+        return api(originalRequest); // try original request again with new token
+      } catch (error) {
+        localStorage.removeItem('accessToken');
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api
   // mek haraha thama api call krnne
